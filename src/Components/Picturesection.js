02f761f21dd9fcd0c6c4978d1947f96b589a4d02
Kira Green <kira.green@btinternet.com>
2019-04-05 15:16:06 +0100

    added touches

--- src/Components/Picturesection.js
+++ src/Components/Picturesection.js
@@ -5,6 +5,7 @@ export default function Picturesection({ title, subtitle, dark, id }) {
 	return (
 		<div className={"section" + (dark ? " section-dark" : "")}>
 			<div className="section-content" id={id}>
+				<h1>{title}</h1>
 				<Pictures />
 			</div>
 		</div>
