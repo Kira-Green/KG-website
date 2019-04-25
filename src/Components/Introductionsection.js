b5f23c770d4f1b3879b9514d5341ff297b7f8bd5
Kira Green <kira.green@btinternet.com>
2019-04-25 13:35:28 +0100

    centered image and text

--- src/Components/Introductionsection.js
+++ src/Components/Introductionsection.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Introduction from "../Components/Introduction";
-import Home from '../Components/Home'
+import Home from "../Components/Home";
 
 export default function Introductionsection({ title, subtitle, dark, id }) {
 	return (
-		<div className='introsection'>
+		<div className="introsection">
 			<div className="section-content" id={id}>
-				<h1 className='home-name'>{title}</h1>
+				<h1 className="home-name">{title}</h1>
 				<Home />
-                
 			</div>
+			<p>Photo: Solar Eclipse, Oregon 21 August 2017 Kira Green</p>
 		</div>
 	);
 }
