ca73bc64614efe6c801c7868f8a137fc64a2302e
Kira Green <kira.green@btinternet.com>
2019-04-26 14:42:43 +0100

    updated links

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,36 +1,43 @@
 import React, { Component } from "react";
-import linkedin from "../../Images/linkedin.png"
-import medium from '../../Images/medium.png'
-import github from '../../Images/GitHub_Logo.png'
-import css from './contact.module.css'
-
-
-
-
-
-
+import linkedin from "../../Images/linkedin.png";
+import medium from "../../Images/medium.png";
+import github from "../../Images/GitHub_Logo.png";
+import css from "./contact.module.css";
 
 const Contact = () => {
-    return (
-      <div className={css.contact}>
-      {/* <p>Check out my blog on Medium and my work on GitHub. You can contact me via Linkedin or email.</p> */}
-      <a href="https://medium.com/@kira.green" target="_blank" ><img className ={css.medium} src= {medium}/></a>
-      <br />
-      <br />
+	return (
+		<div className={css.contact}>
+			{/* <p>Check out my blog on Medium and my work on GitHub. You can contact me via Linkedin or email.</p> */}
+			<a href="https://medium.com/@kira.green" target="_blank">
+				<img className={css.medium} src={medium} />
+			</a>
+			<br />
+			<br />
 
+			<a href="https://github.com/Kira-Green" target="_blank">
+				<img className={css.github} src={github} />{" "}
+			</a>
 
-      <a href='https://github.com/Kira-Green' target='_blank'><img className={css.github} src={github}/> </a>
+			<br />
+			<br />
 
-      <br/>
-      <br />
-      
-      <a href='https://www.linkedin.com/in/kira-green-70500b92/' target='_blank' className={css.linkedin} ><img src={linkedin}/> </a>
-      <br />
-      <br />
-      <p>Email me: <a href="mailto:kira.green@btinternet.com">kira.green@btinternet.com </a> </p>
-             
-      </div>
-    );
-  };
-  
-  export default Contact;
+			<a
+				href="https://www.linkedin.com/in/kira-green-70500b92/"
+				target="_blank"
+				className={css.linkedin}
+			>
+				<img src={linkedin} />{" "}
+			</a>
+			<br />
+			<br />
+			<div className={css.email}>
+				Email me:{" "}
+				<a href="mailto:kira.green@btinternet.com">
+					kira.green@btinternet.com{" "}
+				</a>{" "}
+			</div>
+		</div>
+	);
+};
+
+export default Contact;
