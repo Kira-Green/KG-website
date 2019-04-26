d38dd5f85781eed12ac26fd710c31cbf2642f196
Kira Green <kira.green@btinternet.com>
2019-04-26 13:57:56 +0100

    skills section done

--- src/Components/Introductionsection.js
+++ src/Components/Introductionsection.js
@@ -4,12 +4,14 @@ import Home from "../Components/Home";
 
 export default function Introductionsection({ title, subtitle, dark, id }) {
 	return (
-		<div className="introsection" id={id}>
-			<div className="homeName">Kira Green</div>
-			<div className="homeJob">Web Developer </div>
-			<p className="photocredit">
-				Photo: Solar Eclipse, Oregon 21 August 2017 Kira Green
-			</p>
+		<div className="homeContainer">
+			<div className="introsection" id={id}>
+				<div className="homeName">Kira Green</div>
+				<div className="homeJob">Web Developer </div>
+				<p className="photocredit">
+					Photo: Solar Eclipse, Oregon 21 August 2017 Kira Green
+				</p>
+			</div>
 		</div>
 	);
 }
--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -1,28 +1,48 @@
 import React, { Component } from "react";
-import css from './skills.module.css'
-
-
+import css from "./skills.module.css";
+import cssIcon from "../../Images/css.svg";
+import htmlIcon from "../../Images/html.svg";
+import jsIcon from "../../Images/javascript.svg";
+import nodeIcon from "../../Images/node.svg";
+import reactIcon from "../../Images/react.svg";
+import expressIcon from "../../Images/expressjs-icon.svg";
+import css3Icon from "../../Images/css3.svg";
+import gitIcon from "../../Images/git.svg";
+import githubIcon from "../../Images/github.svg";
+import mongoIcon from "../../Images/mongodb.svg";
 
 const Skills = () => {
-    return (
-      <div className="skills">
-         <h3>Technical Skills    </h3>
-      <ul className={css.educationlist}>
-          <li>    HTML</li>
-          <li>    CSS</li>
-          <li>    JavaScript</li>
-          <li>    Node.js</li>
-          <li>    React.js</li>
-          <li>    Express.js</li>
-          <li>    GitHub</li>
-          <li>    Agile Development</li>
-          <li>    ZenHub</li>
-          <li>    MongoDB</li>
-      </ul>
-        
-      </div>
-    );
-  };
-  
-  export default Skills;
-  
+	return (
+		<div className={css.skills}>
+			<div className={css.css3}>
+				<img className={css.icons} src={css3Icon} />{" "}
+			</div>
+			<div className={css.html}>
+				<img className={css.icons} src={htmlIcon} />
+			</div>
+			<div className={css.js}>
+				<img className={css.icons} src={jsIcon} />
+			</div>
+			<div className={css.node}>
+				<img className={css.icons} src={nodeIcon} />
+			</div>
+			<div className={css.react}>
+				<img className={css.icons} src={reactIcon} />
+			</div>
+			<div className={css.express}>
+				<img className={css.icons} src={expressIcon} />
+			</div>
+			<div className={css.git}>
+				<img className={css.icons} src={gitIcon} />
+			</div>
+			<div className={css.github}>
+				<img className={css.icons} src={githubIcon} />
+			</div>
+			<div className={css.mongo}>
+				<img className={css.icons} src={mongoIcon} />
+			</div>
+		</div>
+	);
+};
+
+export default Skills;
